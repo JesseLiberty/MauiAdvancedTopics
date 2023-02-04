23763631a4044c115e8de767f01a97156ddcfdd1
jesse liberty <jesse.liberty@cnhind.com>
2023-02-04 14:47:26 -0500

    Add id to preferences

--- ForgetMeNotDemo/ForgetMeNotDemo/Model/Preference.cs
+++ ForgetMeNotDemo/ForgetMeNotDemo/Model/Preference.cs
@@ -5,6 +5,7 @@ namespace ForgetMeNotDemo.Model;
 [ObservableObject]
 public partial class Preference
 {
+    [ObservableProperty] private int id;
     [ObservableProperty] private string preferencePrompt;
     [ObservableProperty] private string preferenceValue;
 
